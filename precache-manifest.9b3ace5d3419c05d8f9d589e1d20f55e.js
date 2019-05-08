3c82986389bec5d7d3a47f08d7a4106770f0deb7
[Dorijan] <[dorijan11@gmail.com]>
2019-05-09 00:44:25 +0200

    Updates

---
+++ precache-manifest.9b3ace5d3419c05d8f9d589e1d20f55e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7345582c5820d5bd3fbe967f7e4d4569",
+    "url": "/xdorijanx/CountrySearch/index.html"
+  },
+  {
+    "revision": "9f4109c221df0b3fb07d",
+    "url": "/xdorijanx/CountrySearch/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/xdorijanx/CountrySearch/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9f4109c221df0b3fb07d",
+    "url": "/xdorijanx/CountrySearch/static/js/main.0b06d9f0.chunk.js"
+  },
+  {
+    "revision": "30e56aed4382960e7ab0",
+    "url": "/xdorijanx/CountrySearch/static/js/runtime~main.4eb63311.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xdorijanx/CountrySearch/static/media/logo.5d5d9eef.svg"
+  }
+]);
